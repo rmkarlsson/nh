f93a5e0c6fd97861b1a48a522729cc0ca1c211d7
Mikael Karlsson <rmkarlsson@protonmail.ch>
2026-08-09 13:32:02 +0000

    [task] Make it run in HA

--- main.py
+++ main.py
@@ -14,26 +14,21 @@ logger = logging.getLogger(__name__)
 
 
 class EnergyPriceApp(Hass):
-  def __init__(self, ad=None, name=None, *args, **kwargs):
-    self.ad = ad
-    self.active_entity = kwargs.pop("active_entity", "input_boolean.heating_active")
-    self.name = name or "energy_price_app"
-    if ad is None:
-      super().__init__(*args, **kwargs)
-      self.name = name or self.__class__.__name__
-    else:
-      self.initialize()
-
   def initialize(self):
-    if self.ad is not None:
-      self.ad.run_daily(self._run, datetime.time(23, 55, 0))
-    else:
-      self.run_daily(self._run, datetime.time(23, 55, 0))
+    self.active_entity = self.args.get(
+        "active_entity",
+        "input_boolean.heating_active"
+    )
+
+    self.run_daily(
+        self._run,
+        datetime.time(23, 55, 0)
+    )
 
   def _run(self, *args, **kwargs):
     main(app=self, active_entity=self.active_entity)
 
-  def schedule_activation(self, start_dt, end_dt, active_entity="switch.0xf0fd45fffeef4457"):
+  def schedule_activation(self, start_dt, end_dt, active_entity=None):
     # AccTankKontaktor == 0xf0fd45fffeef4457
     entity_id = active_entity or self.active_entity
     if entity_id is None:
@@ -58,21 +53,22 @@ class EnergyPriceApp(Hass):
       logger.info("Sätter %s till %s", entity_id, "på" if active else "av")
 
 
+
 def parse_period(value):
-  if isinstance(value, datetime.datetime):
-    return value
-  if not isinstance(value, str):
-    return None
+    if isinstance(value, datetime.datetime):
+        return value
 
-  try:
-    parsed = datetime.datetime.fromisoformat(value.replace("Z", "+00:00"))
-  except ValueError:
-    return None
+    if not isinstance(value, str):
+        return None
 
-  if parsed.tzinfo is not None:
-    parsed = parsed.astimezone(datetime.timezone.utc).replace(tzinfo=None)
-  return parsed
+    try:
+        parsed = datetime.datetime.fromisoformat(
+            value.replace("Z", "+00:00")
+        )
+    except ValueError:
+        return None
 
+    return parsed
 
 def main(elomrade="SE2", active_entity="input_boolean.heating_active", app=None):
   today = datetime.date.today()
